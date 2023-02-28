0f158f2fae241391dce48df8b08dbfcb58a6bcea
srujith <srujithreddy801@gmail.com>
2023-02-28 21:36:15 +0530

    Files modified

--- cgpa.py
+++ cgpa.py
@@ -1,10 +1,29 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request, jsonify
+from werkzeug.utils import secure_filename
+import os
+
+
+
+
 app=Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
-@app.route('/')
+
+
+@app.route('/',methods=['POST','GET'])
 def home():
     return render_template('app.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return render_template('upload.html', filename=filename)
+
+
 
 
 
